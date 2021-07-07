007a33521fc7586f930b6569575254b96e880d63
Daniel Cardoso <cardosodaniel75@gmail.com>
2021-07-07 10:59:32 -0300

    2o commit no hotfix

--- Git-Aula-Gerencia-Conf/src/HelloWorld.java
+++ Git-Aula-Gerencia-Conf/src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		System.out.println("Hello World!!!");
 		System.out.println("Mudando pelo Bash");
 		System.out.println("Hotfix criado aqui");
+		System.out.println("Realizando segundo commit no Hotfix");
 	}
 
 }
